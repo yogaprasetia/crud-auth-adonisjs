46d794d71705921c5c1bfa568ae486d453bb39d0
yogaprasetia <yogapraseetia@gmail.com>
2024-02-02 11:33:51 +0700

    first commit

---
+++ start/routes.js
@@ -0,0 +1,35 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+/**
+* register
+*/
+Route.get('register', 'Auth/RegisterController.index').as('register.index').middleware(['RedirectIfAuthenticated'])
+Route.post('register', 'Auth/RegisterController.store').as('register.store').middleware(['RedirectIfAuthenticated'])
+
+/**
+* login
+*/
+Route.get('login', 'Auth/LoginController.index').as('login.index').middleware(['RedirectIfAuthenticated'])
+Route.post('login', 'Auth/LoginController.check').as('login.check').middleware(['RedirectIfAuthenticated'])
+Route.get('logout', 'Auth/LoginController.logout').as('logout').middleware(['Authenticate'])
+
+/**
+* dashboard
+*/
+Route.get('dashboard', 'DashboardController.index').as('dashboard').middleware(['Authenticate'])
